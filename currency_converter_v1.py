77e7ddf008ddddcf0828f3a673ff30d4a5654109
ZYM <zwdnet@163.com>
2017-12-07 00:19:17 +0800

    第二天，汇率兑换

---
+++ currency_converter_v1.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+"""小象学院python教程
+汇率兑换"""
+
+rmb_str_value = input("请输入人民币(CNY)金额:")
+rmb_value = eval(rmb_str_value)
+usd_vs_rmb = 6.77
+usd_value = rmb_value / usd_vs_rmb
+print("可兑换的美元为:", usd_value)
+
