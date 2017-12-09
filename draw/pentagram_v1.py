3851c3bedaaad9e06e649973fa08e5b824c4add2
ZYM <zwdnet@163.com>
2017-12-09 20:14:06 +0800

    第五天，分形，绘制五角星。turtle库。

---
+++ draw/pentagram_v1.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+"""
+绘制五角星
+版本:1.0
+日期:20171209
+"""
+import turtle
+
+
+def main():
+    for i in range(6):
+        turtle.forward(300)
+        turtle.right(144)
+    turtle.exitonclick()
+
+
+
+if __name__ == '__main__':
+    main()
