2bd779fbed80bef4add34d58b025fe8de8d6dad0
ZYM <zwdnet@163.com>
2017-12-17 21:12:52 +0800

    第十二天，密码强度判断，break和continue语句，限制尝试次数。

---
+++ password/pwd_strength_v2.py
@@ -0,0 +1,67 @@
+# -*- coding:utf-8 -*-
+"""
+判断密码强度 break和continue语句，限制密码尝试次数
+20171217
+v2.0
+"""
+
+
+# 判断字符串中是否含有数字
+def check_number_exist(password_str):
+    has_number = False
+    for c in password_str:
+        if c.isnumeric():
+            has_number = True
+            break
+    return has_number
+
+
+def check_letter_exist(password_str):
+    has_letter = False
+    for c in password_str:
+        if c.isalpha():
+            has_letter = True
+            break
+    return has_letter
+
+
+def main():
+    try_times = 5
+
+    while try_times > 0:
+
+        password = input("请输入密码:")
+        # 密码强度变量
+        strength_level = 0
+
+        # 规则一 密码长度大于8
+        if len(password) >= 8:
+            strength_level += 1
+        else:
+            print("密码长度至少8位")
+
+        # 包涵数字
+        if check_number_exist(password):
+            strength_level += 1
+        else:
+            print("密码要求包含数字！")
+
+        # 包涵字母
+        if check_letter_exist(password):
+            strength_level += 1
+        else:
+            print("密码要求包含字母！")
+
+        if strength_level == 3:
+            print("密码强度合格")
+            break
+        else:
+            print("密码强度过低，请重新设定。")
+            try_times -= 1
+    if try_times <= 0:
+        print("尝试次数过多，密码设置失败")
+    else:
+        print("密码设置成功")
+
+if __name__ == "__main__":
+    main()
